5150ac1a423482a691b7e0a42e054c57c50155ce
jayllellis <jay@52pick-up.com>
2014-07-29 10:52:47 -0400

    Added custom js files and instructions

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ foundation-wp-starter
 =====================
 
 Starter WP Theme built on Foundation Framework
+
+- The theme includes a `myplugins.js` (where you place all your plugin scripts) and a `myscripts.js` (where you place all your custom scripts) - Located in the `js` folder
+
+- Feel free to delete the content in these files and start from scratch
---
+++ js/myscripts.js
@@ -0,0 +1,43 @@
+/*************************************************************************************
+*  The Functions
+**************************************************************************************/
+
+/**
+  * @desc checks if current element exists
+  * @param none
+  * @return int - the number of elements present, > 0 = element exists
+*/
+jQuery.fn.exists = function(){ return this.length > 0; }
+
+/**
+  * @desc Adds browser class to html
+  * @param none
+  * @return none
+*/
+function addBrowserClass(){
+  
+  var chrome = $.browser.mozilla();
+  var firefox = $.browser.firefox();
+  var safari = $.browser.safari();
+
+  if (chrome){ $('html').addClass('chrome'); }
+  if (firefox){ $('html').addClass('firefox'); }
+  if (safari){ $('html').addClass('safari'); }
+
+}
+
+/*************************************************************************************
+*  Document ready
+**************************************************************************************/
+
+$(document).ready (function(){
+
+	//addBrowserClass();
+
+});
+
+/*************************************************************************************
+*  After Document Loads
+**************************************************************************************/
+$(window).load (function(){
+});
