9be0643dcd1fe636da137592635a6e21a89559c2
jayllellis <jay@52pick-up.com>
2014-11-24 15:11:03 -0500

    Added 'isFullyOnScreen()' jQuery method

--- js/myscripts.js
+++ js/myscripts.js
@@ -53,6 +53,34 @@ jQuery.fn.isOnScreen = function(){
      
 }
 
+/**
+  * @desc checks if current element is 100% visible within viewport
+  * @param none
+  * @return int - the coordinates of the window vs. the element
+*/
+jQuery.fn.isFullyVisible = function(){
+       
+    var win = $(window);
+       
+    var viewport = {
+        top : win.scrollTop(),
+        left : win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+      
+    var elemtHeight = this.height();// Get the full height of current element
+    elemtHeight = Math.round(elemtHeight);// Round it to whole humber
+       
+    var bounds = this.offset();// Coordinates of current element
+    bounds.top = bounds.top + elemtHeight;// Top is redefined as half of current element's height
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+             
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+     
+}
+
 /*--------------------------------------------------------------
 DOCUMENT READY
 --------------------------------------------------------------*/
