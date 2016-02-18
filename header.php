4fc47512281816f45ba47d35ff728d6ed424820b
Neilgebbie <n_gebbie@live.com>
2016-02-18 10:14:15 -0500

    official site adjustment

--- header.php
+++ header.php
@@ -7,24 +7,25 @@
   <!-- <link rel="shortcut icon" href="" type="image/png"> -->
   <title>
 		<?php
-			/*
-			 * Print the <title> tag based on what is being viewed.
-			 */
-			global $page, $paged;
-			
-			wp_title( '|', true, 'right' );
-		
-			// Add the blog name.
-			bloginfo( 'name' );
-		
-			// Add the blog description for the home/front page.
-			$site_description = get_bloginfo( 'description', 'display' );
-			if ( $site_description && ( is_home() || is_front_page() ) )
-				echo " | $site_description";
-		
-			// Add a page number if necessary:
-			if ( $paged >= 2 || $page >= 2 )
-				echo ' | ' . sprintf( __( 'Page %s', THEME_TEXTDOMAIN ), max( $paged, $page ) );
+            /*
+             * Print the <title> tag based on what is being viewed.
+             */
+            global $page, $paged;
+            
+            wp_title( '|', true, 'right' );
+        
+            // Add the blog name.
+            //bloginfo( 'name' );
+        
+            // Add the blog description for the home/front page.
+            $site_description = get_bloginfo( 'description', 'display' );
+            if ( $site_description && ( is_home() || is_front_page() ) )
+        
+            // Add a page number if necessary:
+            if ( $paged >= 2 || $page >= 2 )
+                echo ' | ' . sprintf( __( 'Page %s', THEME_TEXTDOMAIN ), max( $paged, $page ) );
+
+      echo " | $site_description";
 		?>
   </title>
   
